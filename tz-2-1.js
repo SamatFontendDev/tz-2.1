e29012e6d4f98a2a413ff365665cba7bd87e5bc4
Samatt1991 <samat116rust@yandex.ru>
2019-09-30 18:20:53 +0300

    tz2.1 start

---
+++ tz-2-1.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+const rimraf = require('rimraf');
+const baseArg = process.argv[2];
+const levaelArg = process.argv[3];
+const summaryFolderArg = process.argv[4];
+const removeSourceArg = process.argv[5];
+
+const readDir = (base, level, removeSource, summaryFolder) => {
+    const files = fs.readdirSync(base);
+
+    
+}
