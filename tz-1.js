90051a4ebd3e68bce029ea182a7445766a60cbc1
Samatt1991 <samat116rust@yandex.ru>
2019-09-30 18:04:33 +0300

    initial commit

---
+++ tz-1.js
@@ -0,0 +1,63 @@
+const fs = require('fs');
+const path = require('path');
+const rimraf = require('rimraf');
+const baseArg = process.argv[2];
+const levaelArg = process.argv[3];
+const summaryFolderArg = process.argv[4];
+const removeSourceArg = process.argv[5];
+
+
+
+
+const readDir = (base, level, removeSource, summaryFolder) => {
+  const files = fs.readdirSync(base);
+  fs.access(`${summaryFolder}/myDir`, err => {
+    if (err) {
+      fs.mkdir(`${summaryFolder}/myDir`, () => {
+        console.log('myDir created!');
+      });
+    }
+  });
+  
+  files.forEach(item => {
+    let localBase = path.join(base, item);
+    
+    let state = fs.statSync(localBase);
+    let dirName = item[0];
+    if (state.isDirectory()) {
+      readDir(localBase, level + 1);
+    } else {
+      fs.access(`${summaryFolder}/myDir/${dirName}`, (err) => {
+        if (err) {
+          fs.mkdir(`${summaryFolder}/myDir/${dirName}`, () => {
+            
+            fs.link(localBase, `${summaryFolder}/myDir/${dirName}/${item}`, err => {
+              if (err) {
+                console.log(`${summaryFolder}/myDir/${dirName}/${item}`);
+                console.error(24,err.message);
+              }
+            });
+          });
+
+         } else {
+          fs.link(localBase, `${summaryFolder}/myDir/${dirName}/${item}`, err => {
+            if (err) {
+              console.error(33,err.message);
+            }
+          });
+        }
+      });
+    }
+  });
+  if (removeSource) {
+    rimraf(base, (err) => {
+      if (err) {
+        console.error(50, err.message);
+      } else {
+        console.log('source directory deleted!');
+      }
+    });
+  }
+}
+
+readDir(baseArg, levaelArg, removeSourceArg, summaryFolderArg);
