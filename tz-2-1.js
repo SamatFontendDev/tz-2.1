31db149cdbe5536f5d3732c09754b1afd8062e3e
Samat <47773886+SamatFontendDev@users.noreply.github.com>
2019-10-23 18:39:15 +0300

    2-1 done

--- tz-2-1.js
+++ tz-2-1.js
@@ -1,32 +1,24 @@
+  
 const fs = require('fs');
 const path = require('path');
 const rimraf = require('rimraf');
 const util = require('util');
 
-
 const baseArg = process.argv[2];
 const levaelArg = process.argv[3];
 const summaryFolderArg = process.argv[4];
-const removeSourceArg = process.argv[5];
 
-// const checkDir = util.promisify(fs.stat);
-function checkDir (path) {
-    return new Promise ((resolve, rejected) => {
-        fs.stat(path, function(err, stat) {
-            if(err == null) {
-                resolve(path);
-            } else if(err.code == 'ENOENT') {
-                rejected(err);
-            } 
-        });
-    });
-}
 const createFolder = util.promisify(fs.mkdir);
 const copy = util.promisify(fs.link);
+const checkDir = util.promisify(fs.access);
 
-const readDir = async (base, level, summaryFolder, removeSource) => {
+const readDir = async (base, level, summaryFolder) => {
     const files = fs.readdirSync(base);
-    
+    try {
+        await checkDir('./dir');
+    } catch(err) {
+        createFolder('./dir');
+    }
     
     files.forEach(async item => {
         let localBase = path.join(base, item);
@@ -38,36 +30,15 @@ const readDir = async (base, level, summaryFolder, removeSource) => {
             readDir(localBase, level + 1);
         } else {
             try{
-                await createFolder(`${summaryFolder}/${dirName}`);
-                await copy(localBase, `${summaryFolder}/${dirName}/${item}`);
+                await createFolder(`./dir/${dirName}`);
+                await copy(localBase, `./dir/${dirName}/${item}`);
             } catch(err) {
-                await copy(localBase, `${summaryFolder}/${dirName}/${item}`);
+                await copy(localBase, `./dir/${dirName}/${item}`);
             }
         }
     });
 
 }
-// (async function(){
-//     try{
-//         // await checkDir(`${summaryFolder}/myDir`);
-//       const checkDir2 = await checkDir(`/myDir`);
-//       console.log(58,checkDir2);
-     
-//     } catch(err) {
-      
-//       await createFolder(`./myDir`);
-//     }
-// })()
 
 
-readDir(baseArg, levaelArg, summaryFolderArg, removeSourceArg);
-
-// if (removeSource) {
-//     rimraf(base, (err) => {
-//       if (err) {
-//         console.error(50, err.message);
-//       } else {
-//         console.log('source directory deleted!');
-//       }
-//     });
-// }
+readDir(baseArg, levaelArg, summaryFolderArg);
